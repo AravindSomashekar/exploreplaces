e8b651df974d20dac4087ef550ed490ee439a7b0
aravind <aravinds369@gmail.com>
2024-04-10 10:34:55 +0530

    added header

---
+++ src/app/src/header/header.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header.component';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    HeaderComponent
+  ]
+})
+export class HeaderModule { }
