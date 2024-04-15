0483eecbbbff22c345f6214a7b337f00a9107813
aravind <aravinds369@gmail.com>
2024-04-16 00:10:55 +0530

    added other components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", loadChildren: ()=> import('../app/home/home.module').then(m => m.HomeModule)  },
+  { path: "about", loadChildren: ()=> import('../app/about/about.module').then(m => m.AboutModule)  },
+  { path: "articles", loadChildren: ()=> import('../app/articles/about.module').then(m => m.ArticlesModule)  },
+  { path: "", redirectTo: "home", pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from '../app/header/header.component'
+import { HeaderComponent } from '../app/header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ArticlesComponent } from './articles/articles.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    ArticlesComponent
   ],
   imports: [
     BrowserModule,
