5044a1af29719da62a369ef3c0250f3e2dba0ce6
aravind <aravinds369@gmail.com>
2024-04-16 00:37:58 +0530

    added other components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: "home", loadChildren: ()=> import('../app/home/home.module').then(m => m.HomeModule)  },
-  { path: "about", loadChildren: ()=> import('../app/about/about.module').then(m => m.AboutModule)  },
-  { path: "articles", loadChildren: ()=> import('../app/articles/about.module').then(m => m.ArticlesModule)  },
-  { path: "", redirectTo: "home", pathMatch: 'full' }
+  { path: "home", loadChildren: () => import('../app/home/home.module').then(m => m.HomeModule) },
+  { path: "about", loadChildren: () => import('../app/about/about.module').then(m => m.AboutModule) },
+  { path: "articles", loadChildren: () => import('../app/articles/about.module').then(m => m.ArticlesModule) },
+  { path: "", loadChildren: () => import('../app/home/home.module').then(m => m.HomeModule) },
+  { path: "", redirectTo: "home", pathMatch: 'full' },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
